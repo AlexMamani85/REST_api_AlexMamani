a5e0d9edf46ad0792fed042b38e9ae6410ab1e81
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-10-17 10:23:26 -0500

    CHORE: routes in index.js

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req,res) =>{
     res.send(`Node and Express server is running on port: ${PORT}`)
-})
+});
+
+app.post('/', (req,res) =>{
+    res.send(`Post method`)
+});
+
+app.put('/', (req,res) =>{
+    res.send(`put method`)
+});
+
+app.delete('/', (req,res) =>{
+    res.send(`delete method`)
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`)   
