509782fd4fc79d13a00a20112e8bd11c17bd835f
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-10-17 11:09:06 -0500

    _TEST: trying next method

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require('./src/routes/crmRoutes')
 
-routes(app);
+// app.use(function(req,res,next){
+//     console.log('Time', Date.now);
+// })
+
+app.get('/', function(req, res, next){
+    console.log('Req Method', req.method)
+    next();
+}, function(req, res, next){
+    console.log('Req Original Url', req.originalUrl)
+    next();
+}, function(req, res, next){
+    res.send('Req was successfull')    
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`)   
