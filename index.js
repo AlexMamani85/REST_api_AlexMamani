11d01b3d4849e1e95280e93cc33a60c3512cc7ad
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-10-17 22:51:25 -0500

    CHORE: post blog

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 const app = express();
 const PORT = process.env.PORT || 3000;
-const routes = require('./src/routes/crmRoutes')
+const routes = require('./src/routes/crmRoutes');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/test', {
     useNewUrlParser:true
 });
 
-const Cat = mongoose.model('Cat', {name: String})
-const kitty = new Cat({name: 'mimi'});
-kitty.save().then((res)=>{
-    console.log(res);
-    console.log('Meowwwww');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+const BlogSchema = require('./src/models/crmModel');
+const blogModel = mongoose.model('blog', BlogSchema);
+
+app.post('/newBlog', (req, res) =>{
+    let blog = new blogModel(req.body);
+    blog.save((err, blogModel)=>{
+        if(err) {
+            res.send(err);
+        }
+        res.json(blog);
+    })
 })
 
-// app.use(function(req,res,next){
-//     console.log('Time', Date.now);
-// })
 
-app.get('/', function(req, res, next){
-    console.log('Req Method', req.method)
-    next();
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`)   
